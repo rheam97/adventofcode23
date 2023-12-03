688b980570bb146590668147a395f7d6dc3cf92b
rheam97 <rheam97@gmail.com>
2023-12-02 21:52:17 -0500

    day2 attempt

---
+++ Day 2/index.js
@@ -0,0 +1,62 @@
+function solution(){   
+    const fs = require("fs")
+  
+    // Creating a function which takes a file as input
+    const readFileLines = filename =>
+      fs
+        .readFileSync(filename)
+        .toString('UTF8')
+        .split('\n');
+     
+    // Driver code
+    let data = readFileLines('input.txt');
+    let results = 0
+  
+    for(let i=0; i<data.length; i++){
+      let game = data[i].split(':')
+    
+      let id = parseInt(game[0].split(' ')[1])
+     
+  
+      let subsets = game[1].split(';')
+     
+      
+      /**'3 blue, 4 red', '1 red, 2 green, 6 blue', '2 green' */
+  
+      let blues = 0
+      let reds = 0
+      let greens = 0
+  
+      for(let j =0; j<subsets.length; j++){
+        let set = subsets[j].split(',')
+        
+        for(let l=0; l<set.length; l++){
+          let count = set[l].trim().split(' ')
+          let [num, color] = count
+          
+          if(color === 'blue'){
+            blues= Math.max(blues, parseInt(num))
+          } 
+          else if(color==='red'){
+            reds= Math.max(reds, parseInt(num))
+  
+          }
+          else if(color==='green'){
+            greens= Math.max(greens, parseInt(num))
+  
+          }
+        }
+  
+      }
+  
+  
+      if(blues <= 14 && greens <=13 && reds <=12){
+        results+=id
+      }
+  
+  
+    }
+  
+  }
+
+  solution()
